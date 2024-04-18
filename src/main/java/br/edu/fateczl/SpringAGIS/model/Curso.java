5d8dfd431194e29a8674dd2dbf4aa1a4dbeacda3
luiza <luiza@DESKTOP-82K6BJH>
2024-04-17 23:36:26 -0300

    transformando projeto agis em um projeto spring

---
+++ src/main/java/br/edu/fateczl/SpringAGIS/model/Curso.java
@@ -0,0 +1,18 @@
+package br.edu.fateczl.SpringAGIS.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Curso {
+	int codigo;
+	String nome;
+	int cargaHoraria;
+	String sigla;
+	int notaEnade;
+}
---
+++ src/main/java/br/edu/fateczl/SpringAGIS/persistence/MatriculaDisciplinaDao.java
@@ -0,0 +1,99 @@
+package br.edu.fateczl.SpringAGIS.persistence;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.fateczl.SpringAGIS.model.Disciplina;
+import br.edu.fateczl.SpringAGIS.model.Matricula;
+import br.edu.fateczl.SpringAGIS.model.MatriculaDisciplinas;
+
+public class MatriculaDisciplinaDao implements IDisciplina{
+	private GenericDao gDao;
+	
+	public MatriculaDisciplinaDao(GenericDao gDao) {
+		this.gDao = gDao;
+	}
+
+	@Override
+	public String inserirMatricula(String ra, int codigoMatricula, int codigoDisciplina) throws SQLException, ClassNotFoundException {
+		Connection c = gDao.getConnection();
+		String sql = "CALL sp_inserirmatricula(?,?,?,?)";
+		CallableStatement cs = c.prepareCall(sql);
+		cs.setString(1, ra);
+		cs.setInt(2, codigoMatricula);
+		cs.setInt(3, codigoDisciplina);
+		cs.registerOutParameter(4, Types.VARCHAR);
+		cs.execute();
+		String saida = cs.getString(4);
+		cs.close();
+		c.close();
+		return saida;
+	}
+
+	@Override
+	public List<MatriculaDisciplinas> listarSituacao(String alunoRa)
+			throws SQLException, ClassNotFoundException {
+		List<MatriculaDisciplinas> ms = new ArrayList<>();
+		Connection c = gDao.getConnection();
+		String sql = "SELECT * FROM dbo.fn_listarultimamatricula(?) ORDER BY nome ASC";
+		PreparedStatement ps = c.prepareStatement(sql);
+		ps.setString(1, alunoRa);
+		ResultSet rs = ps.executeQuery();
+		while(rs.next()) {
+			System.out.println(rs.getInt("codigo_matricula"));
+			MatriculaDisciplinas m = new MatriculaDisciplinas();
+			Disciplina d = new Disciplina();
+			d.setCodigo(rs.getInt("codigo"));
+			d.setNome(rs.getString("nome"));
+			d.setQtdAulas(rs.getInt("qtd_aulas"));
+			d.setHorarioInicio(rs.getString("horario_inicio"));
+			d.setHorarioFim(rs.getString("horario_fim"));
+			d.setDiaSemana(rs.getString("dia"));
+			d.setCursoCodigo(rs.getInt("curso_codigo"));
+			m.setDisciplina(d);
+			m.setCodigoMatricula(rs.getInt("codigo_matricula"));
+			m.setSituacao(rs.getString("situacao"));
+			ms.add(m);
+		}
+		
+		ps.close();
+		c.close();
+		return ms;
+	}
+
+	@Override
+	public String gerarMatricula(String alunoRa) throws SQLException, ClassNotFoundException {
+		Connection c = gDao.getConnection();
+		String sql = "CALL sp_gerarmatricula (?,?)";
+		CallableStatement cs = c.prepareCall(sql);
+		cs.setString(1, alunoRa);
+		cs.registerOutParameter(2, Types.INTEGER);
+		cs.execute();
+		String saida = Integer.toString(cs.getInt(2));
+		cs.close();
+		c.close();
+		return saida;
+	}
+
+	@Override
+	public Matricula consultarUltimaMatricula(String alunoRa) throws SQLException, ClassNotFoundException {
+		Connection c = gDao.getConnection();
+		String sql = "SELECT TOP 1 codigo, aluno_ra, data_matricula FROM matricula WHERE aluno_ra = ? ORDER BY codigo DESC";
+		PreparedStatement ps = c.prepareStatement(sql);
+		ps.setString(1, alunoRa);
+		ResultSet rs = ps.executeQuery();
+		Matricula m = new Matricula();
+		if(rs.next()) {
+			m.setCodigo(rs.getInt("codigo"));
+			m.setDataMatricula(rs.getString("data_matricula"));
+			m.setAlunoRa(rs.getString("aluno_ra"));
+		}
+		return m;
+	}
+}
